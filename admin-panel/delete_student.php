d0a268a018c9dadea03ea639c60bc352214d3c43
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-06 15:45:43 +0530

    Update delete_student.php

--- admin-panel/delete_student.php
+++ admin-panel/delete_student.php
@@ -6,6 +6,6 @@ $id = $_GET['id'];
 $conn->query("DELETE FROM students WHERE student_id = $id");
 
 // Redirect
-header("Location: manage_students.php");
+header("Location: manage_student.php");
 exit;
 ?>
