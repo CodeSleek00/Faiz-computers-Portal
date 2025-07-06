7ce2274b468c43a6e5b3e834a813ff8f02bdc075
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-06 14:30:28 +0530

    recreating portal

--- admin_panels/delete_student.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../database_connection/db_connect.php';
-$id = $_GET['id'];
-
-// Optional: delete photo from folder if needed
-$conn->query("DELETE FROM my_student WHERE student_id = $id");
-
-header("Location: view_students.php");
-exit();
