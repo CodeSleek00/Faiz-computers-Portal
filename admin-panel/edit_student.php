06e75ce552b45e42106217c33dd35f7a21b44bfa
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-06 15:32:42 +0530

    Update edit_student.php

--- admin-panel/edit_student.php
+++ admin-panel/edit_student.php
@@ -1,5 +1,5 @@
 <?php
-include 'db_connect.php';
+include '../database_connection/db_connect.php';
 
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
     echo "<p style='color:red;'>❌ Invalid student ID.</p>";
