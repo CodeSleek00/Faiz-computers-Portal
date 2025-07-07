39ba187505b6482b6eb6d236a4813d9d2aa74bce
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-07 11:25:07 +0530

    Update admin_assignment_dashboard.php

--- assignment/admin_assignment_dashboard.php
+++ assignment/admin_assignment_dashboard.php
@@ -316,7 +316,7 @@ $pending_submissions = $conn->query("SELECT COUNT(*) as total FROM assignment_su
                     <p class="action-desc">Organize students into learning groups</p>
                 </a>
 
-                <a href="../batch/view_students.php" class="action-card">
+                <a href="../admin-panel/view_students.php" class="action-card">
                     <div class="action-icon">
                         <i class="fas fa-user-graduate"></i>
                     </div>
