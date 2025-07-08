1eed9ecfcf171b6f8a58af5f0620bb91159f4c8a
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-08 16:37:03 +0530

    Update delete_material.php

--- study-center/delete_material.php
+++ study-center/delete_material.php
@@ -8,9 +8,9 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
 $material_id = intval($_GET['id']);
 
 // Delete material
-$conn->query("DELETE FROM study_material WHERE id = $material_id");
+$conn->query("DELETE FROM study_materials WHERE id = $material_id");
 $conn->query("DELETE FROM study_material_targets WHERE material_id = $material_id");
 
-header("Location: view_material_admin.php?msg=deleted");
+header("Location: view_materials_admin.php?msg=deleted");
 exit;
 ?>
