aea1c94e9b4e4c0a832ce7837462c7d9ea66e01b
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-16 11:01:57 +0530

    Update student_dashboard.php

--- assignment/student_dashboard.php
+++ assignment/student_dashboard.php
@@ -31,7 +31,7 @@ $assignments = $conn->query($assignment_sql);
 <html>
 <head>
     <link rel="icon" type="image/png" href="image.png">
-  <link rel="apple-touch-icon" href="image.png">
+    <link rel="apple-touch-icon" href="image.png">
     <title>Student Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
