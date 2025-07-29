3f3c6614e8a58b9b4cdb0db3538b67a0c6504fdd
CodeSleek00 <codesleekstudio@gmail.com>
2025-07-29 23:37:17 +0530

    Update reassign_exam.php

--- exam-center/admin/reassign_exam.php
+++ exam-center/admin/reassign_exam.php
@@ -33,6 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html>
 <head>
     <title>Re-Assign Exam</title>
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <style>
         body {
             font-family: 'Poppins', sans-serif;
