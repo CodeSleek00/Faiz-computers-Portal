5d085a0983eb620081289082de6938956be17cd6
CodeSleek00 <codesleekstudio@gmail.com>
2025-09-09 21:56:08 +0530

    Update fee_receipt.php

--- fee/fee_receipt.php
+++ fee/fee_receipt.php
@@ -124,7 +124,7 @@ $labels = [
 
   <div class="text-center mt-3 no-print">
       <button class="btn btn-primary" onclick="window.print()">🖨 Print Receipt</button>
-      <a class="btn btn-secondary" href="admin_fee_dashboard.php ?>">Back</a>
+      <a class="btn btn-secondary" href="admin_fee_dashboard.php">Back</a>
   </div>
 </div>
 </body>
