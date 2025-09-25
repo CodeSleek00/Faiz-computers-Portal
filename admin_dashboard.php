b4ba7dfd54a8a345d1a3bab4fac99c8758fb480d
CodeSleek00 <codesleekstudio@gmail.com>
2025-09-25 19:26:13 +0530

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -483,7 +483,7 @@ $total_materials = $conn->query("SELECT COUNT(*) AS c FROM study_materials")->fe
                     <i class="fa-solid fa-receipt"></i>
                     <span>Fee</span>
                 </a>
-                <a href="video-portal/admin_videos.php" class="menu-item">
+                <a href="video-portal/admin_video_dashboard.php" class="menu-item">
                    <i class="fa-solid fa-video"></i>
                     <span>Video Upload</span>
                 </a>
