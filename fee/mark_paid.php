50e4dbb81f01c729185c881f06b772e89aafde9d
CodeSleek00 <codesleekstudio@gmail.com>
2025-12-07 18:28:22 +0530

    c

--- fee/mark_paid.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db_connect.php";
-
-$fee_id = $_GET['id'];
-$conn->prepare("UPDATE fee_master SET status='paid' WHERE id=?")->execute([$fee_id]);
-
-header("Location: " . $_SERVER['HTTP_REFERER']);
-exit;
-?>
