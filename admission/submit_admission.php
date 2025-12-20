e22db5b8ecca36703f58a92a2f3094ff386bf25b
CodeSleek00 <codesleekstudio@gmail.com>
2025-12-20 21:47:43 +0530

    Update submit_admission.php

--- admission/submit_admission.php
+++ admission/submit_admission.php
@@ -23,7 +23,7 @@ $duration = (int)($_POST['duration'] ?? 0);
 $registration_fee = (float)($_POST['registration_fee'] ?? 0);
 $monthly_fee      = (float)($_POST['per_month_fee'] ?? 0);
 $internal_fee     = (float)($_POST['internal_fee'] ?? 0);
-$semester_fee     = (float)($_POST['semester_fee'] ?? 0);
+$semester_exam_fee     = (float)($_POST['semester_fee'] ?? 0);
 $additional_fee   = (float)($_POST['additional_fee'] ?? 0);
 
 // ================= IMAGE UPLOAD =================
@@ -69,7 +69,7 @@ mysqli_query($conn,"
 INSERT INTO admission
 (name,aadhar,apaar,phone,email,religion,caste,address,permanent_address,
 dob,photo,father_name,mother_name,parent_contact,course_name,duration,
-registration_fee,per_month_fee,internal_fee,semester_fee,additional_fee,enrollment_id)
+registration_fee,per_month_fee,internal_fee,semester_exam_fee,additional_fee,enrollment_id)
 VALUES
 ('$name','$aadhar','$apaar','$phone','$email','$religion','$caste','$address','$permanent_address',
 '$dob','$photo_name','$father_name','$mother_name','$parent_contact','$course_name','$duration',
