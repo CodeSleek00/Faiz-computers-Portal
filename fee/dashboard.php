fb780012757c5cba40abff7504f2b6487b678d86
CodeSleek00 <codesleekstudio@gmail.com>
2025-12-24 09:49:36 +0530

    Update dashboard.php

--- fee/dashboard.php
+++ fee/dashboard.php
@@ -2,25 +2,30 @@
 include("db_connect.php");
 
 $students = $conn->query("
-    SELECT DISTINCT enrollment_id, name, photo, course_name
+    SELECT DISTINCT enrollment_id, name, photo, course_name, admission_date
     FROM student_monthly_fee
-    ORDER BY name ASC
+    ORDER BY admission_date DESC
 ");
 ?>
 
-<table border="1" width="100%">
+<table border="1" width="100%" cellpadding="10">
 <tr>
     <th>Photo</th>
     <th>Name</th>
     <th>Course</th>
+    <th>Admission Date</th>
     <th>Action</th>
 </tr>
 
 <?php while($s = $students->fetch_assoc()): ?>
 <tr>
-    <td><img src="../uploads/<?= $s['photo'] ?>" width="50"></td>
-    <td><?= $s['name'] ?></td>
-    <td><?= $s['course_name'] ?></td>
+    <td>
+        <img src="../uploads/<?= $s['photo'] ?: 'no-photo.png' ?>" width="50"
+             onerror="this.src='../assets/no-photo.png'">
+    </td>
+    <td><?= htmlspecialchars($s['name']) ?></td>
+    <td><?= htmlspecialchars($s['course_name']) ?></td>
+    <td><?= date("d-M-Y", strtotime($s['admission_date'])) ?></td>
     <td>
         <a href="student_fee_select.php?enroll=<?= $s['enrollment_id'] ?>">
             <button>Submit Fee</button>
