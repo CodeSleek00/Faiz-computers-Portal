e1de305e89f54784ffbf58d200ec251978b67a72
CodeSleek00 <codesleekstudio@gmail.com>
2025-12-30 20:43:40 +0530

    Update dashboard.php

--- video-portal/student/dashboard.php
+++ video-portal/student/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../database_connection/db_connect.php';
+include '../../database_connection/db_connect.php';
 if(!isset($_SESSION['enrollment_id'])){ header("Location: ../login/login.php"); exit; }
 
 $student_id = $_SESSION['student_id'];
