f90e1d66baeb6f064ce9771232e84e7e761d80cb
CodeSleek00 <codesleekstudio@gmail.com>
2025-12-30 20:13:35 +0530

    Update session_check.php

--- video-portal/includes/session_check.php
+++ video-portal/includes/session_check.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['user_type'])) {
-    header("Location: ../login-system/login.php");
+    header("Location: ../../login-system/login.php");
     exit;
 }
 ?>
