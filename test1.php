0111bc8ce45d8a63232940040759465c389e131b
CodeSleek00 <codesleekstudio@gmail.com>
2026-04-04 22:36:16 +0530

    Update test1.php

--- test1.php
+++ test1.php
@@ -98,33 +98,6 @@ $student = $result->fetch_assoc();
         </div>
     </div>
 </div>
-<div class="reader">
-    <h1>
-        this is a reader page where im checking about the term 
-        and condition of the admission form and also checking about the signature and also checking about the photo and also checking about the name and also checking about the batch id and also checking about the course and also checking about the enrollment id and also checking about the student id and also checking about the student table and also checking about the database connection and also checking about the session and also checking about the login check and also checking about the student info from session and also checking about the fetch student details and also checking about the prepare statement and also checking about the bind param and also checking about the execute statement and also checking about the get result and also checking about the fetch assoc and also checking about the html structure and also checking about the css styling and also checking about the print media query and also checking about the print button and also checking about the header section and also checking about the photo container and also checking about the section title and also checking about the table structure and also checking about the td styling and also checking about the signature table styling and also checking about the sign space styling and also checking about the footer note styling
-    </h1>
-    <div class="class">
-        and this is a div where the class is defined and the class name is class
-        and the data is shifting on the own of their own data is reasoning andthe thing is youre  makiing up a 
-        story and the story is about the student and the student is a good student and the student is a hard working student and the student is a smart student and the student is a intelligent student and the student is a brilliant student and the student is a talented student and the student is a creative student and the student is a innovative student and the student is a passionate student and the student is a dedicated student and the student is a motivated student and the student is a ambitious student and the student is a confident student and the student is a responsible student and the student is a disciplined student and the student is a organized student and the student is a punctual student and the student is a respectful student and the student is a honest student and the student is a loyal student and the student is a trustworthy student and the student is a reliable student and the student is a supportive student and the student is a helpful student and the student is a friendly student and the student is a kind-hearted student
-    </div>
-    <div class="new">creating new object on their own to make it more 
-        the qucik brown fox over the lazy dog 
-        the qucik brown fox over the lazy dog 
-        the 
-        <script>
-            a=Number(prompt("Enter the number"))
-            b=Number(prompt("Enter the number"))
-            if a % 2 ==0 {
-                console.log("the number is even ")
-            }
-             else {
-                console.log("the number is odd")
-            }
-            first $name=prompt("Enter your first name")
-            last $name=prompt("Enter your last name")
-        </script>
-    </div>
-</div>
+
 </body>
 </html>
